a44e0d394c9a2755d778e1c0d53651fdb71bb407
roadf010 <58123715+roadf010@users.noreply.github.com>
2024-04-10 21:26:57 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ searchInput.addEventListener("input", (e) => {
     else{
         equations.forEach(EQ =>{
             if (splitval.length == 1){
-                isVisible = value in EQ.name.toLowerCase() || value in EQ.units.toLowerCase() || value in EQ.type.toLowerCase() || value in EQ.field.toLowerCase() || value in EQ.results.toLowerCase()
+                isVisible = EQ.name.toLowerCase().includes(value) || EQ.units.toLowerCase().includes(value) || EQ.type.toLowerCase().includes(value) || EQ.field.toLowerCase().includes(value) || EQ.results.toLowerCase().includes(value)
             }
             else{
                 isVisible = value in EQ.name.toLowerCase()
@@ -157,7 +157,7 @@ searchInput.addEventListener("input", (e) => {
                         console.log("testing")
                         console.log(splitval[0])
                         console.log(splitval[1])
-                        isVisible = ((splitval[0] in EQ.field.toLowerCase()) && (splitval[1] in EQ.type.toLowerCase())) || (splitval[0] in EQ.type.toLowerCase() && splitval[1] in EQ.field.toLowerCase()) || (splitval[0] in EQ.units.toLowerCase() && splitval[1] in EQ.type.toLowerCase()) || (splitval[0] in EQ.type.toLowerCase() && splitval[1] in EQ.units.toLowerCase()) || (splitval[0] in EQ.field.toLowerCase() && splitval[1] in EQ.units.toLowerCase()) || (splitval[0] in EQ.units.toLowerCase() && splitval[1] in EQ.field.toLowerCase()) 
+                        isVisible = (EQ.field.toLowerCase().includes(splitval[0]) && EQ.type.toLowerCase().includes(splitval[1])) || (EQ.type.toLowerCase().includes(splitval[0]) && EQ.field.toLowerCase().includes(splitval[1])) || (EQ.units.toLowerCase().includes(splitval[0]) && EQ.type.toLowerCase().includes(splitval[1])) || (EQ.type.toLowerCase().includes(splitval[0]) && EQ.units.toLowerCase().includes(splitval[1])) || (EQ.field.toLowerCase().includes(splitval[0]) && EQ.units.toLowerCase().includes(splitval[1])) || (EQ.units.toLowerCase().includes(splitval[0]) && EQ.field.toLowerCase().includes(splitval[1])) 
                         if (isVisible == true){
                             console.log("breaking second")
                             break
