719dad69d78a2194ba1f1512006217e34afeb2d1
roadf010 <58123715+roadf010@users.noreply.github.com>
2024-04-18 15:04:50 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -171,15 +171,15 @@ searchInput.addEventListener("input", (e) => {
     if (value == "" || value == null || !(value.includes("+") || value.includes("-") || value.includes("*") || value.includes("/") || value.includes("^") || matches != null)){
         calculations.forEach(calc =>{
             calc.element.classList.toggle("hide", true)
+            calc.element.classList.style.backgroundColor = "rgb(63, 252, 46)"
         })
-        calc.element.classList.style.backgroundColor = "rgb(63, 252, 46)"
     } 
     else if (value.includes("+") || value.includes("-") || value.includes("*") || value.includes("/") || value.includes("^") || matches != null){
         console.log("performing calculation")
         calculations.forEach(calc =>{
             calc.element.classList.toggle("hide", false)
+            calc.element.classList.style.backgroundColor = "rgb(46, 252, 218)"
         })
-        calc.element.classList.style.backgroundColor = "rgb(46, 252, 218)"
         var result;
         valid = true;
         result = calc_phys_val(value)
